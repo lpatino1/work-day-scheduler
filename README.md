053dc27e8af8c010f6f430c43f5824cc36a88efa
Lesli <lpatino0994@gmail.com>
2022-07-05 22:23:53 -0500

    added to jquery, added to README

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # work-day-scheduler
+
+## Description
+A work day scheduler to plan your work day! Set times from 9am to 5pm. You enter your events in the text area then press the save button to save it locally. Even if you refresh, your saved events will remain in the corresponding time block. Each time block will be colorcoded depending on the current hour. If it is in the past, the time block will be grey. If it is in the future, it will be green. If it's the current hour, it will be red.
+
+Currently, there is a minor bug where the color will stretch past the button.
+
+## Installation
+No Installation Needed
+
+## Usage
+You may run on Visual Studio Code or any other platform.
+
+## Images
+
+## Link to Deployed Site
+
+## Collaboration
+
+Collaborated with classmate Noah and Jovi.
+
--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-//global variables
 
 //we will use this to set the color of the blocks depending on the current hour
 var timeEl = $('.time');
 
-var workDay = [
-    ''
-]
 
 //Display current day and date
 var dayDate = moment().format("dddd, MMMM do YYYY");
@@ -13,21 +9,132 @@ var dayDate = moment().format("dddd, MMMM do YYYY");
 $("#currentDay").text(dayDate);
 
 //Current Hour
-var currentHour = moment().format("h");
+var currentHour =  moment().format("HH");
 console.log(currentHour);
 
 //Check hours in planner relative to the current hour: present, past, future
 
-function checkHour(hour, currentHour){
-    
-    console.log(timeEl.value);
+$(".time").each(function(){
+ var timeBlock = $(this).attr("id");
+  console.log(this);
+ if(timeBlock<currentHour){
+    $(this).addClass("past");
+ } else if(timeBlock==currentHour){
+  $(this).addClass("present");
+ } else if (timeBlock>currentHour){
+  $(this).addClass("future");
+ }
+})
 
-} 
+//global variables
+var text1El = $('#nine');
+var text2El = $('#ten');
+var text3El = $('#eleven');
+var text4El = $('#twelve');
+var text5El = $('#thirteen');
+var text6El = $('#fourteen');
+var text7El = $('#fifteen');
+var text8El = $('#sixteen');
+var text9El = $('#seventeen');
 
-checkHour();
 
-//save to local storage, add function to ALL save buttons
-$("button").on("click", function(){
-    //save to local storage
+//button handlers
+var ninebtnEl = $('#ninebtn');
+var tenbtnEl = $('#tenbtn');
+var elevenbtnEl = $('#elevenbtn');
+var twelvebtnEl = $('#twelvebtn');
+var thirteenbtnEl = $('#thirteenbtn');
+var fourteenbtnEl = $('#fourteenbtn');
+var fifteenbtnEl = $('#fifteenbtn');
+var sixteenbtnEl = $('#sixteenbtn');
+var seventeenbtnEl = $('#seventeenbtn');
+
+
+//on-click handlers
+(ninebtnEl).on("click", function(e){
+  //prevent default (when a page refreshes the stuff clears so stop it)
+  e.preventDefault();
+  let item = $(text1El).val();
+  console.log(item);    
+  localStorage.setItem("nine", item);
+});
+
+
+$(tenbtnEl).on("click", function(e){
     //prevent default (when a page refreshes the stuff clears so stop it)
-  });
+    e.preventDefault();
+    let item = $(text2El).val();
+    console.log(item);    
+    localStorage.setItem("ten", item);
+});
+
+$(elevenbtnEl).on("click", function(e){
+    //prevent default (when a page refreshes the stuff clears so stop it)
+    e.preventDefault();
+    let item = $(text3El).val();
+    console.log(item);    
+    localStorage.setItem("eleven", item);
+});
+
+$(twelvebtnEl).on("click", function(e){
+    //prevent default (when a page refreshes the stuff clears so stop it)
+    e.preventDefault();
+    let item = $(text4El).val();
+    console.log(item);    
+    localStorage.setItem("twelve", item);
+});
+
+$(thirteenbtnEl).on("click", function(e){
+    //prevent default (when a page refreshes the stuff clears so stop it)
+    e.preventDefault();
+    let item = $(text5El).val();
+    console.log(item);    
+    localStorage.setItem("thirteen", item);
+});
+
+$(fourteenbtnEl).on("click", function(e){
+    //prevent default (when a page refreshes the stuff clears so stop it)
+    e.preventDefault();
+    let item = $(text6El).val();
+    console.log(item);    
+    localStorage.setItem("fourteen", item);
+});
+
+$(fifteenbtnEl).on("click", function(e){
+    //prevent default (when a page refreshes the stuff clears so stop it)
+    e.preventDefault();
+    let item = $(text7El).val();
+    console.log(item);    
+    localStorage.setItem("fifteen", item);
+});
+
+$(sixteenbtnEl).on("click", function(e){
+    //prevent default (when a page refreshes the stuff clears so stop it)
+    e.preventDefault();
+    let item = $(text8El).val();
+    console.log(item);    
+    localStorage.setItem("sixteen", item);
+});
+
+$(seventeenbtnEl).on("click", function(e){
+    //prevent default (when a page refreshes the stuff clears so stop it)
+    e.preventDefault();
+    let item = $(text9El).val();
+    console.log(item);    
+    localStorage.setItem("seventeen", item);
+});
+
+//display previous saved items
+function renderToDo(){
+    text1El.text(localStorage.getItem("nine"));
+    text2El.text(localStorage.getItem("ten"));
+    text3El.text(localStorage.getItem("eleven"));
+    text4El.text (localStorage.getItem("twelve"));
+    text5El.text (localStorage.getItem("thirteen"));
+    text6El.text (localStorage.getItem("fourteen"));
+    text7El.text ( localStorage.getItem("fifteen"));
+    text8El.text ( localStorage.getItem("sixteen"));
+    text9El.text ( localStorage.getItem("seventeen"));
+}
+
+renderToDo();
